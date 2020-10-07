ed779f3dc9f055c72178581b631050dddc7f1dc5
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-10-07 17:22:33 +0200

    feat: base project scaffolding

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div style="height: 50vh; width: 50vw;">
+    <l-map
+      v-model="zoom"
+      v-model:zoom="zoom"
+      :center="[47.41322, -1.219482]"
+      @move="log('move')"
+    >
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      ></l-tile-layer>
+      <l-marker
+        :lat-lng="[0, 0]"
+        draggable
+        @moveend="log('moveend')"
+      ></l-marker>
+    </l-map>
+  </div>
+</template>
+<script>
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data() {
+    return {
+      zoom: 2,
+    };
+  },
+  methods: {
+    log(a) {
+      console.log(a);
+    },
+  },
+};
+</script>
